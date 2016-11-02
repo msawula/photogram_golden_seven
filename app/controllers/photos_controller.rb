b8dba189306f44c6cdafba20cdfca3e8c06887b2
Michael Sawula <msawula@users.noreply.github.com>
2016-11-01 21:04:34 -0500

    perfect answers

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -26,7 +26,21 @@ redirect_to("http://localhost:3000/photos")
 end
 
 def destroy
+p = Photo.find_by({ :id => params[:id]})
+p = p.destroy
+redirect_to("http://localhost:3000/photos")
+end
 
+def edit_form
+  p = Photo.find_by({ :id => params[:id]})
+end
+
+def update_row
+  p = Photo.find_by({ :id => params[:id]})
+  p.caption = params[:the_caption]
+  p.source = params[:the_source]
+  p.save
+redirect_to("/photos/#{@pid}")
 end
 
 end
